6309f7f67585470f530436852dbbc6c00902c353
Astazium <113135541+Astazium@users.noreply.github.com>
2025-10-25 17:53:12 +0300

    Патч звуков
    Чтобы не дублировались на клиенте

--- modules/init/client.lua
+++ modules/init/client.lua
@@ -40,6 +40,44 @@ function initializator.init_packs()
     external_app.load_content()
 end
 
+-- Патч чтоб звуки не дублировались, ну и может для ещё чего то
+function initializator.patch_packs()
+    local blacklist = { "audio" }
+
+    local safe_env = {}
+    for k, v in pairs(_G) do
+        safe_env[k] = v
+    end
+
+    for _, lib_name in ipairs(blacklist) do
+        logger.log(string.format("patching %s library", lib_name), nil, nil, "init/client.lua")
+        safe_env[lib_name] = setmetatable({}, {
+            __index = function(t, key)
+                return function() end
+            end,
+            __call = function()
+            end
+        })
+    end
+
+    local function matches_content_pack(key)
+        for _, pack in ipairs(CONTENT_PACKS) do
+            if string.find(key, pack .. ":", 1, true) then
+                return true
+            end
+        end
+        return false
+    end
+
+    for event_name, handlers in pairs(events.handlers) do
+        if matches_content_pack(event_name) then
+            for i, func in ipairs(handlers) do
+                setfenv(func, safe_env)
+            end
+        end
+    end
+end
+
 -- Инициализация скриптов
 function initializator.init_pack_scripts()
     local paths = file.list_all_res("scripts/client/")
@@ -52,4 +90,5 @@ function initializator.init_pack_scripts()
 end
 
 initializator.init_packs()
+initializator.patch_packs()
 initializator.init_pack_scripts()
